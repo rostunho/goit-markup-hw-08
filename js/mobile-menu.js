40319832261d168609e7c77f5799b2f9b8cd8473
Rostyk Podolets <ros.podolets@gmail.com>
2021-07-03 08:29:57 +0300

    v.0.5.
    header and mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,30 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.header__mobile-menu-button'),
+    closeMenuBtn: document.querySelector('.mobile-menu__close-button'),
+    menu: document.querySelector('.mobile-menu'),
+  };
 
-// (() => {
-//   const mobileMenu = document.querySelector('[data-mobile-menu]');
-//   const openMenuBtn = document.querySelector('[data-menu-open]');
-//   const closeMenuBtn = document.querySelector('[data-menu-close]');
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-//   const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//     mobileMenu.classList.toggle('mobile-menu--is-open');
-
-//     const scrollLockMethod = !isMenuOpen
-//       ? 'disableBodyScroll'
-//       : 'enableBodyScroll';
-//     bodyScrollLock[scrollLockMethod](document.body);
-//   };
-
-//   openMenuBtn.addEventListener('click', toggleMenu);
-//   closeMenuBtn.addEventListener('click', toggleMenu);
-
-//   // Закрываем мобильное меню на более широких экранах
-//   // в случае изменения ориентации устройства.
-//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//     if (!e.matches) return;
-//     mobileMenu.classList.remove('is-open');
-//     openMenuBtn.setAttribute('aria-expanded', false);
-//     bodyScrollLock.enableBodyScroll(document.body);
-//   });
-// })();
+  function toggleMenu() {
+    refs.menu.classList.toggle('mobile-menu--is-hidden');
+  }
+})();
